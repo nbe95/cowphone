e6da34582d536e3519cf883444679852fcd2a088
Niklas Bettgen <niklas@bettgen.de>
2024-05-15 15:47:40 +0200

    Catch empty history

--- static/api/api.js
+++ static/api/api.js
@@ -8,8 +8,8 @@ const loadVersion = async () => {
 
 const loadHistory = async () => {
   const request = await fetch("/api/v1/history", { method: "GET" });
-  try {
-    const result = await request.json();
+  const result = await request.json();
+  if (result.length) {
     let html = "";
     result.forEach((cow) => {
       html += `<div class="col col-12 col-sm-6 col-md-4 col-lg-3">
@@ -22,8 +22,7 @@ const loadHistory = async () => {
       </div>`;
     });
     document.getElementById("cowphone-history").innerHTML = html;
-  } catch (error) {
-    console.error(error);
+  } else {
     document.getElementById("cowphone-history").innerHTML =
       `<div class="alert alert-warning d-flex flex-row" role="alert">
         <span class="me-auto">Apparently there's no cow power yet...</span>
